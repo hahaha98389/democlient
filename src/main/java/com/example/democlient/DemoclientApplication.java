6a1e154c909c43e867f0db0b2b41bc98b95a62d6
hahaha98389 <359075446@qq.com>
2020-01-06 15:30:37 +0800

    初始化

--- src/main/java/com/example/democlient/DemoclientApplication.java
+++ src/main/java/com/example/democlient/DemoclientApplication.java
@@ -3,6 +3,8 @@ package com.example.democlient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @SpringBootApplication
@@ -10,6 +12,10 @@ public class DemoclientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoclientApplication.class, args);
+    }
+    @Bean
+    public RestTemplate getBean(){
+        return new RestTemplate();
     }
 
 }
--- src/main/java/com/example/democlient/controller/FeignController.java
+++ src/main/java/com/example/democlient/controller/FeignController.java
@@ -1,11 +1,13 @@
-package com.example.democlient.controller.impl;
+package com.example.democlient.controller;
 
 import com.example.democlient.bean.Person;
-import com.example.democlient.controller.interfaces.PersonInterface;
+import com.example.democlient.service.interfaces.PersonInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/FeignController")
 public class FeignController {
     @Autowired
