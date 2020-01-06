2db7681184792f188036d367a5a6273a108cba57
hahaha98389 <359075446@qq.com>
2020-01-06 16:46:34 +0800

    初始化

--- src/main/java/com/example/democlient/DemoclientApplication.java
+++ src/main/java/com/example/democlient/DemoclientApplication.java
@@ -2,10 +2,14 @@ package com.example.democlient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
+@EnableHystrix
 @EnableFeignClients
 @SpringBootApplication
 public class DemoclientApplication {
